d422adf05fbed28d34abec0d463877e9eb71d585
Ankit kumar <99538470+AkankitAk@users.noreply.github.com>
2023-02-26 01:59:22 +0530

    Delete WeeklyTest_4 directory

--- WeeklyTest_4/src/main/java/com/weeklyTest/ecommerceapi/dao/ProductRepo.java
+++
@@ -1,9 +0,0 @@
-package com.weeklyTest.ecommerceapi.dao;
-
-import com.weeklyTest.ecommerceapi.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepo extends JpaRepository<Product,Integer> {
-}

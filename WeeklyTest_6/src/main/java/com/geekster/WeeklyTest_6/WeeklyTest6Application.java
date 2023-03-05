fd621136550e21086c0c3e8e86f5bc75751ddac0
Ankit <akankitag@gmail.com>
2023-03-05 17:11:51 +0530

    6th test

---
+++ WeeklyTest_6/src/main/java/com/geekster/WeeklyTest_6/WeeklyTest6Application.java
@@ -0,0 +1,13 @@
+package com.geekster.WeeklyTest_6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeeklyTest6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeeklyTest6Application.class, args);
+	}
+
+}
---
+++ WeeklyTest_6/src/test/java/com/geekster/WeeklyTest_6/WeeklyTest6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geekster.WeeklyTest_6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeeklyTest6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
